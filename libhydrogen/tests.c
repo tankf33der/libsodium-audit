0ec27b56d7a9577b8b784996e9c9e94424fad493
Mike <mpech@tuta.io>
2020-03-09 09:24:33 +0200

    6ct0h1

--- libhydrogen/tests.c
+++ libhydrogen/tests.c
@@ -482,8 +482,8 @@ main(void)
     //ret = hydro_init();
     //assert(ret == 0);
 
-    test_core();
-    //test_hash();
+    //test_core();
+    test_hash();
 
     //test_kdf();
     //test_kx_n();
